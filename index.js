69368f3e4559374576dc44816b4675f8300c73e7
sgh4123@naver.com <sgh4123@naver.com>
2023-10-10 01:37:11 +0900

    데이터베이스에 한자 데이터 집어넣음

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs-extra');
+const csv = require('csv-parser');
 const Database = require('better-sqlite3');
 const db = new Database('./db/jplang.db');
+db.exec(`DROP TABLE IF EXISTS jpchars`);
 db.exec(`CREATE TABLE IF NOT EXISTS jpchars (id INTEGER PRIMARY KEY, hanza TEXT, mean_sound TEXT)`);
 const server = require('http').createServer(app);
 
@@ -10,6 +13,22 @@ const port = 80;
 
 import("node-fetch");
 
+function initDatabase(){
+    fs.createReadStream('./db/sangyong.csv')
+      .pipe(csv({
+        separator: ','
+      }))
+      .on('data', (row) => {
+        let cursor = db.prepare(`insert into jpchars (hanza, mean_sound) values (?, ?)`);
+        console.log(row.hanza);
+        cursor.run(row.hanza, row.mean_sound);
+        })
+      .on('end', () => {
+        console.log("데이터 삽입 완료");
+      });
+}
+
+initDatabase();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
