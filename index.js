b73a7aaec9308775ee721ee90f25516412ada4df
sgh4123@naver.com <sgh4123@naver.com>
2023-10-10 00:39:52 +0900

    객관식 주관식 html 생성 및 기타 작업

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const server = require('http').createServer(app);
 const port = 80;
 
+import("node-fetch");
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(path.join(__dirname, "./public/html/home.html"));
 });
 
-app.listen(port, () => {
+app.get('/multiple', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/html/multiple.html"));
+});
+
+app.get('/single', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/html/single.html"));
+});
+
+app.get('/home', (req, res) => {
+    res.redirect('/');
+})
+
+app.use("/api/char", require("./routes/character"));
+
+server.listen(port, () => {
     console.log("서버 실행중입니다.");
 })
